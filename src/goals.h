3d8c94958a9cd30954ab20d877e93ac7755a8bee
rexim <reximkut@gmail.com>
2018-01-17 00:43:13 +0700

    Add TODO about level hiding mechanism (#58)

--- src/goals.h
+++ src/goals.h
@@ -6,6 +6,8 @@
 typedef struct goals_t goals_t;
 typedef struct SDL_Renderer SDL_Renderer;
 
+// TODO: implement goal hiding mechanism
+
 goals_t *create_goals_from_stream(FILE *stream);
 void destroy_goals(goals_t *goals);
 
