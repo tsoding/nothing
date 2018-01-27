4280b1830c7c4a99e83014e8697d5396c2b2b465
rexim <reximkut@gmail.com>
2018-01-28 02:01:07 +0700

    Make triangle rendering funcs accept triangle_t (#87)

--- src/renderer.h
+++ src/renderer.h
@@ -2,18 +2,15 @@
 #define RENDERER_H_
 
 #include "./point.h"
+#include "./triangle.h"
 
 typedef struct SDL_Renderer SDL_Renderer;
 
 int draw_triangle(SDL_Renderer *render,
-                  point_t p1,
-                  point_t p2,
-                  point_t p3);
+                  triangle_t t);
 
 int fill_triangle(SDL_Renderer *render,
-                  point_t p1,
-                  point_t p2,
-                  point_t p3);
+                  triangle_t t);
 
 
 #endif  // RENDERER_H_
