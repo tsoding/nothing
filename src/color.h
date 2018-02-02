288e4c5f85fbe9de47761ecdac68feb2219338e7
rexim <reximkut@gmail.com>
2018-02-02 22:35:11 +0700

    Introduce color256 constructor (#98)

--- src/color.h
+++ src/color.h
@@ -8,6 +8,7 @@ typedef struct color_t {
 } color_t;
 
 color_t color(float r, float g, float b, float a);
+color_t color256(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
 SDL_Color color_for_sdl(color_t color);
 
 color_t color_desaturate(color_t color);
