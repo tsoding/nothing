482753d3788fa28425a6edc8348d2c830c81ed25
rexim <reximkut@gmail.com>
2018-02-15 23:17:08 +0700

    (#102) Implement the player death

--- src/player.c
+++ src/player.c
@@ -9,11 +9,13 @@
 #include "./point.h"
 #include "./error.h"
 #include "./rigid_rect.h"
+#include "./dying_rect.h"
 
 #define PLAYER_WIDTH 25.0f
 #define PLAYER_HEIGHT 25.0f
 #define PLAYER_SPEED 500.0f
 #define PLAYER_JUMP 550.0f
+#define PLAYER_DEATH_DURATION 500
 
 typedef enum player_state_t {
     PLAYER_STATE_ALIVE = 0,
@@ -23,8 +25,13 @@ typedef enum player_state_t {
 struct player_t {
     lt_t *lt;
     player_state_t state;
-    rigid_rect_t *player_body;
+
+    rigid_rect_t *alive_body;
+    dying_rect_t *dying_body;
+
     int jump_count;
+    color_t color;
+    vec_t initial_position;
 };
 
 player_t *create_player(float x, float y, color_t color)
@@ -42,18 +49,32 @@ player_t *create_player(float x, float y, color_t color)
     }
 
     player->state = PLAYER_STATE_ALIVE;
-    player->player_body = PUSH_LT(
+
+    player->alive_body = PUSH_LT(
         lt,
         create_rigid_rect(
             rect(x, y, PLAYER_WIDTH, PLAYER_HEIGHT),
             color),
         destroy_rigid_rect);
-    if (player->player_body == NULL) {
+    if (player->alive_body == NULL) {
+        RETURN_LT(lt, NULL);
+    }
+
+    player->dying_body = PUSH_LT(
+        lt,
+        create_dying_rect(
+            rect(x, y, PLAYER_WIDTH, PLAYER_HEIGHT),
+            color,
+            PLAYER_DEATH_DURATION),
+        destroy_dying_rect);
+    if (player->dying_body == NULL) {
         RETURN_LT(lt, NULL);
     }
 
     player->lt = lt;
     player->jump_count = 0;
+    player->color = color;
+    player->initial_position = vec(x, y);
 
     return player;
 }
@@ -83,7 +104,18 @@ int player_render(const player_t * player,
     assert(player);
     assert(renderer);
     assert(camera);
-    return rigid_rect_render(player->player_body, renderer, camera);
+
+    switch (player->state) {
+    case PLAYER_STATE_ALIVE:
+        return rigid_rect_render(player->alive_body, renderer, camera);
+
+    case PLAYER_STATE_DYING:
+        return dying_rect_render(player->dying_body, renderer, camera);
+
+    default: {}
+    }
+
+    return 0;
 }
 
 void player_update(player_t *player,
@@ -93,38 +125,73 @@ void player_update(player_t *player,
     assert(player);
     assert(platforms);
 
-    rigid_rect_update(player->player_body, platforms, delta_time);
+    switch (player->state) {
+    case PLAYER_STATE_ALIVE: {
+        rigid_rect_update(player->alive_body, platforms, delta_time);
 
-    if (rigid_rect_touches_ground(player->player_body)) {
-        player->jump_count = 0;
+        if (rigid_rect_touches_ground(player->alive_body)) {
+            player->jump_count = 0;
+        }
+
+        const rect_t hitbox = rigid_rect_hitbox(player->alive_body);
+
+        if (hitbox.y > 1000.0f) {
+            player->dying_body = RESET_LT(
+                player->lt,
+                player->dying_body,
+                create_dying_rect(
+                    hitbox,
+                    player->color,
+                    PLAYER_DEATH_DURATION));
+            player->state = PLAYER_STATE_DYING;
+        }
+    } break;
+
+    case PLAYER_STATE_DYING: {
+        dying_rect_update(player->dying_body, delta_time);
+
+        if (dying_rect_is_dead(player->dying_body)) {
+            player->alive_body = RESET_LT(
+                player->lt,
+                player->alive_body,
+                create_rigid_rect(
+                    rect_from_vecs(
+                        player->initial_position,
+                        vec(PLAYER_WIDTH, PLAYER_HEIGHT)),
+                    player->color));
+            player->state = PLAYER_STATE_ALIVE;
+        }
+    } break;
+
+    default: {}
     }
 }
 
 void player_move_left(player_t *player)
 {
     assert(player);
-    rigid_rect_move(player->player_body, vec(-PLAYER_SPEED, 0.0f));
+    rigid_rect_move(player->alive_body, vec(-PLAYER_SPEED, 0.0f));
 }
 
 void player_move_right(player_t *player)
 {
     assert(player);
 
-    rigid_rect_move(player->player_body, vec(PLAYER_SPEED, 0.0f));
+    rigid_rect_move(player->alive_body, vec(PLAYER_SPEED, 0.0f));
 }
 
 void player_stop(player_t *player)
 {
     assert(player);
 
-    rigid_rect_move(player->player_body, vec(0.0f, 0.0f));
+    rigid_rect_move(player->alive_body, vec(0.0f, 0.0f));
 }
 
 void player_jump(player_t *player)
 {
     assert(player);
     if (player->jump_count < 2) {
-        rigid_rect_jump(player->player_body, PLAYER_JUMP);
+        rigid_rect_jump(player->alive_body, PLAYER_JUMP);
         player->jump_count++;
     }
 }
@@ -135,7 +202,7 @@ void player_focus_camera(player_t *player,
     assert(player);
     assert(camera);
 
-    const rect_t player_hitbox = rigid_rect_hitbox(player->player_body);
+    const rect_t player_hitbox = rigid_rect_hitbox(player->alive_body);
 
     camera_center_at(
         camera,
@@ -149,5 +216,5 @@ void player_hide_goals(const player_t *player,
 {
     assert(player);
     assert(goals);
-    goals_hide(goals, rigid_rect_hitbox(player->player_body));
+    goals_hide(goals, rigid_rect_hitbox(player->alive_body));
 }
