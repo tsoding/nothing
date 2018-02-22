6e4802b13a1f931e0511c1027bd57e8e9d82e675
rexim <reximkut@gmail.com>
2018-02-22 22:41:28 +0700

    (#111) parse lava from level file

--- src/lava.c
+++ src/lava.c
@@ -4,20 +4,70 @@
 
 #include "./lt.h"
 #include "./lava.h"
+#include "./error.h"
+#include "./color.h"
 
 struct lava_t {
     lt_t *lt;
+    size_t rects_count;
+    rect_t *rects;
+    color_t *colors;
 };
 
 lava_t *create_lava_from_stream(FILE *stream)
 {
     assert(stream);
-    return NULL;
+
+    lt_t *lt = create_lt();
+    if (lt == NULL) {
+        return NULL;
+    }
+
+    lava_t *lava = PUSH_LT(lt, malloc(sizeof(lava_t)), free);
+    if (lava == NULL) {
+        throw_error(ERROR_TYPE_LIBC);
+        RETURN_LT(lt, NULL);
+    }
+
+    if (fscanf(stream, "%lu", &lava->rects_count) < 0) {
+        throw_error(ERROR_TYPE_LIBC);
+        RETURN_LT(lt, NULL);
+    }
+
+    lava->rects = PUSH_LT(lt, malloc(sizeof(rect_t) * lava->rects_count), free);
+    if (lava->rects == NULL) {
+        throw_error(ERROR_TYPE_LIBC);
+        RETURN_LT(lt, NULL);
+    }
+
+    lava->colors = PUSH_LT(lt, malloc(sizeof(color_t) * lava->rects_count), free);
+    if (lava->colors == NULL) {
+        throw_error(ERROR_TYPE_LIBC);
+        RETURN_LT(lt, NULL);
+    }
+
+    char color[7];
+    for (size_t i = 0; i < lava->rects_count; ++i) {
+        if (fscanf(stream, "%f%f%f%f%6s\n",
+                   &lava->rects[i].x, &lava->rects[i].y,
+                   &lava->rects[i].w, &lava->rects[i].h,
+                   color) < 0) {
+            throw_error(ERROR_TYPE_LIBC);
+            RETURN_LT(lt, NULL);
+        }
+
+        lava->colors[i] = color_from_hexstr(color);
+    }
+
+    lava->lt = lt;
+
+    return lava;
 }
 
 void destroy_lava(lava_t *lava)
 {
     assert(lava);
+    RETURN_LT0(lava->lt);
 }
 
 int lava_render(const lava_t   *lava,
