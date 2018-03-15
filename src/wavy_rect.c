51566f20d578b0ca1cde2858ad6533aeb9bc6f37
rexim <reximkut@gmail.com>
2018-03-16 02:37:05 +0700

    (#122) Basic implementation of wavy_rect_t

--- src/wavy_rect.c
+++ src/wavy_rect.c
@@ -4,24 +4,61 @@
 
 #include <SDL2/SDL.h>
 
-#include "wavy_rect.h"
+#include "./wavy_rect.h"
+#include "./lt.h"
+#include "./error.h"
+
+struct wavy_rect_t
+{
+    lt_t *lt;
+
+    rect_t rect;
+    color_t color;
+};
 
 wavy_rect_t *create_wavy_rect(rect_t rect, color_t color)
 {
-    (void) rect;
-    (void) color;
-    return NULL;
+    lt_t *lt = create_lt();
+    if (lt == NULL) {
+        return NULL;
+    }
+
+    wavy_rect_t *wavy_rect = PUSH_LT(lt, malloc(sizeof(wavy_rect_t)), free);
+    if (wavy_rect == NULL) {
+        throw_error(ERROR_TYPE_SDL2);
+        RETURN_LT(lt, NULL);
+    }
+
+    wavy_rect->rect = rect;
+    wavy_rect->color = color;
+    wavy_rect->lt = lt;
+
+    return wavy_rect;
 }
 
 wavy_rect_t *create_wavy_rect_from_stream(FILE *stream)
 {
     assert(stream);
-    return NULL;
+    char color_name[7];
+    rect_t rect;
+
+    if (fscanf(stream, "%f%f%f%f%6s\n",
+               &rect.x, &rect.y,
+               &rect.w, &rect.h,
+               color_name) < 0) {
+        throw_error(ERROR_TYPE_LIBC);
+        return NULL;
+    }
+
+    color_t color = color_from_hexstr(color_name);
+
+    return create_wavy_rect(rect, color);
 }
 
 void destroy_wavy_rect(wavy_rect_t *wavy_rect)
 {
     assert(wavy_rect);
+    RETURN_LT0(wavy_rect->lt);
 }
 
 int wavy_rect_render(const wavy_rect_t *wavy_rect,
@@ -31,7 +68,11 @@ int wavy_rect_render(const wavy_rect_t *wavy_rect,
     assert(wavy_rect);
     assert(renderer);
     assert(camera);
-    return 0;
+    return camera_fill_rect(
+        camera,
+        renderer,
+        wavy_rect->rect,
+        wavy_rect->color);
 }
 int wavy_rect_update(wavy_rect_t *wavy_rect,
                      Uint32 delta_time)
