cf4e2830fc883615ed46745f26903a7af10bb0f8
rexim <reximkut@gmail.com>
2018-03-16 16:54:41 +0700

    (#117) dummy cxd implemention

--- devtools/cxd/main.c
+++ devtools/cxd/main.c
@@ -2,6 +2,15 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc < 3) {
+        fprintf(stderr, "USAGE: cxd <input-file> <output-file>\n");
+        return -1;
+    }
+
+    char *output_file = argv[2];
+    fclose(fopen(output_file, "w"));
+
     /* TODO: cxd tools is not implemented */
+
     return 0;
 }
