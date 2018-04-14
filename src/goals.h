02e1687fb2c37da2bb805d2a49875a92e1608192
rexim <reximkut@gmail.com>
2018-04-15 03:32:58 +0700

    Remove dead TODOs #64 #65

--- src/goals.h
+++ src/goals.h
@@ -7,9 +7,6 @@
 typedef struct goals_t goals_t;
 typedef struct SDL_Renderer SDL_Renderer;
 
-// TODO(#64): implement goal hiding mechanism
-// TODO(#65): add some glow to the goal to make it stand out
-
 goals_t *create_goals_from_stream(FILE *stream);
 void destroy_goals(goals_t *goals);
 
