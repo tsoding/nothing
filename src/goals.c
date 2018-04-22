d59d3c2dd8c660d3dd9aa5c7b368472f380db8a7
rexim <reximkut@gmail.com>
2018-04-23 01:51:04 +0700

    (#118) Implement cue state machine

--- src/goals.c
+++ src/goals.c
@@ -11,6 +11,16 @@
 
 #define GOAL_RADIUS 10.0f
 
+static int goals_is_goal_hidden(const goals_t *goals, size_t i);
+
+typedef enum cue_state_t {
+    CUE_STATE_VIRGIN = 0,
+    CUE_STATE_PLAY_SOMETHING,
+    CUE_STATE_SOMETHING,
+    CUE_STATE_PLAY_NOTHING,
+    CUE_STATE_NOTHING
+} cue_state_t;
+
 struct goals_t {
     lt_t *lt;
     point_t *points;
@@ -82,7 +92,7 @@ goals_t *create_goals_from_stream(FILE *stream)
             RETURN_LT(lt, NULL);
         }
         goals->colors[i] = color_from_hexstr(color);
-        goals->cue_states[i] = 0;
+        goals->cue_states[i] = CUE_STATE_VIRGIN;
     }
 
     goals->lt = lt;
@@ -132,7 +142,7 @@ int goals_render(const goals_t *goals,
     assert(camera);
 
     for (size_t i = 0; i < goals->goals_count; ++i) {
-        if (!rects_overlap(goals->regions[i], goals->player_hitbox)) {
+        if (!goals_is_goal_hidden(goals, i)) {
             if (goals_render_core(goals, i, renderer, camera) < 0) {
                 return -1;
             }
@@ -164,9 +174,18 @@ int goals_sound(goals_t *goals,
                 sound_medium_t *sound_medium)
 {
     for (size_t i = 0; i < goals->goals_count; ++i) {
-        if (goals->cue_states[i] == 1) {
+        switch (goals->cue_states[i]) {
+        case CUE_STATE_PLAY_SOMETHING:
             sound_medium_play_sound(sound_medium, 1, goals->points[i], 0);
-            goals->cue_states[i] = 2;
+            goals->cue_states[i] = CUE_STATE_SOMETHING;
+            break;
+
+        case CUE_STATE_PLAY_NOTHING:
+            sound_medium_play_sound(sound_medium, 0, goals->points[i], 0);
+            goals->cue_states[i] = CUE_STATE_NOTHING;
+            break;
+
+        default: {}
         }
     }
 
@@ -178,8 +197,35 @@ void goals_cue(goals_t *goals,
                const camera_t *camera)
 {
     for (size_t i = 0; i < goals->goals_count; ++i) {
-        if (goals->cue_states[i] == 0 && camera_is_point_visible(camera, renderer, goals->points[i])) {
-            goals->cue_states[i] = 1;
+        switch (goals->cue_states[i]) {
+        case CUE_STATE_VIRGIN:
+            if (!goals_is_goal_hidden(goals, i) && camera_is_point_visible(camera, renderer, goals->points[i])) {
+                goals->cue_states[i] = CUE_STATE_PLAY_SOMETHING;
+            }
+
+            break;
+
+        case CUE_STATE_SOMETHING:
+            if (goals_is_goal_hidden(goals, i) && camera_is_point_visible(camera, renderer, goals->points[i])) {
+                goals->cue_states[i] = CUE_STATE_PLAY_NOTHING;
+            }
+
+            break;
+
+        case CUE_STATE_NOTHING:
+            if (goals_is_goal_hidden(goals, i) && !camera_is_point_visible(camera, renderer, goals->points[i])) {
+                goals->cue_states[i] = CUE_STATE_VIRGIN;
+            }
+            break;
+
+        default: {}
         }
     }
+}
+
+/* Private Functions */
+
+static int goals_is_goal_hidden(const goals_t *goals, size_t i)
+{
+    return rects_overlap(goals->regions[i], goals->player_hitbox);
 }
