6c5ee0f0719c4f88cb9c7dd8a8cd15a36329c1cd
rexim <reximkut@gmail.com>
2018-04-29 03:49:41 +0700

    (#110) CUE_STATE_NOTHING -> CUE_STATE_SEEN_NOTHING

--- src/goals.c
+++ src/goals.c
@@ -16,7 +16,7 @@ static int goals_is_goal_hidden(const goals_t *goals, size_t i);
 typedef enum cue_state_t {
     CUE_STATE_VIRGIN = 0,
     CUE_STATE_HIT_NOTHING,
-    CUE_STATE_NOTHING
+    CUE_STATE_SEEN_NOTHING
 } cue_state_t;
 
 struct goals_t {
@@ -171,7 +171,7 @@ int goals_sound(goals_t *goals,
         switch (goals->cue_states[i]) {
         case CUE_STATE_HIT_NOTHING:
             sound_medium_play_sound(sound_medium, 0, goals->points[i], 0);
-            goals->cue_states[i] = CUE_STATE_NOTHING;
+            goals->cue_states[i] = CUE_STATE_SEEN_NOTHING;
             break;
 
         default: {}
@@ -194,7 +194,7 @@ void goals_cue(goals_t *goals,
 
             break;
 
-        case CUE_STATE_NOTHING:
+        case CUE_STATE_SEEN_NOTHING:
             if (!goals_is_goal_hidden(goals, i) && camera_is_point_visible(camera, renderer, goals->points[i])) {
                 goals->cue_states[i] = CUE_STATE_VIRGIN;
             }
