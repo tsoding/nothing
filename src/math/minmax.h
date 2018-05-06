36569963342a3c8718ebfacf56a2e7de011efada
rexim <reximkut@gmail.com>
2018-05-07 03:53:21 +0700

    (#184) forward declare max impls

--- src/math/minmax.h
+++ src/math/minmax.h
@@ -1,6 +1,9 @@
 #ifndef MINMAX_H_
 #define MINMAX_H_
 
+inline int64_t min_int64(int64_t a, int64_t b);
+inline int64_t max_int64(int64_t a, int64_t b);
+
 inline int64_t min_int64(int64_t a, int64_t b)
 {
     return a < b ? a : b;
