e842a9b2be0c8b383d11e824dad77fe655289026
rexim <reximkut@gmail.com>
2018-08-26 01:07:40 +0700

    TODO(#293)

--- src/script/parser.c
+++ src/script/parser.c
@@ -141,7 +141,7 @@ void print_parse_error(FILE *stream,
                        const char *str,
                        struct ParseError error)
 {
-    /* TODO: print_parse_error doesn't support colors */
+    /* TODO(#293): print_parse_error doesn't support colors */
     /* TODO: print_parse_error doesn't support multiple lines */
 
     fprintf(stream, "%s\n", str);
