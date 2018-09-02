a17ac8280104d8d027f5dd058ef830e6f2061e0e
rexim <reximkut@gmail.com>
2018-09-02 23:38:58 +0700

    (#311) implement eval

--- src/script/interpreter.c
+++ src/script/interpreter.c
@@ -4,8 +4,53 @@
 
 #include "./interpreter.h"
 
-struct Expr eval(struct Expr expr)
+struct EvalResult eval_success(struct Expr expr)
 {
-    /* TODO(#311): eval(expr) is not implemented */
-    return expr;
+    struct EvalResult result = {
+        .is_error = false,
+        .expr = expr
+    };
+
+    return result;
+}
+
+struct EvalResult eval_failure(const char *error)
+{
+    struct EvalResult result = {
+        .is_error = true,
+        .error = error
+    };
+
+    return result;
+}
+
+static struct EvalResult eval_atom(struct Expr scope, struct Atom *atom)
+{
+    /* TODO: eval_atom is not implemented */
+    assert(atom);
+    (void) scope;
+    return eval_failure("not implemented");
+}
+
+static struct EvalResult eval_funcall(struct Expr scope, struct Cons *cons)
+{
+    /* TODO: eval_funcall is not implemented */
+    assert(cons);
+    (void) scope;
+    return eval_failure("not implemented");
+}
+
+struct EvalResult eval(struct Expr scope, struct Expr expr)
+{
+    switch(expr.type) {
+    case EXPR_ATOM:
+        return eval_atom(scope, expr.atom);
+
+    case EXPR_CONS:
+        return eval_funcall(scope, expr.cons);
+
+    default: {}
+    }
+
+    return eval_failure("eval: unexpected expression");
 }
--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -1,9 +1,22 @@
 #ifndef INTERPRETER_H_
 #define INTERPRETER_H_
 
+#include <stdbool.h>
 #include "expr.h"
 
+struct EvalResult
+{
+    bool is_error;
+    union
+    {
+        struct Expr expr;
+        const char *error;
+    };
+};
 
-struct Expr eval(struct Expr expr);
+struct EvalResult eval_success(struct Expr result);
+struct EvalResult eval_failure(const char *error);
+
+struct EvalResult eval(struct Expr scope, struct Expr expr);
 
 #endif  // INTERPRETER_H_
