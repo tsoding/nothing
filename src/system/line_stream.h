f06ffb4b2b427c308e1d73331b6432b737314344
rexim <reximkut@gmail.com>
2018-10-26 20:38:27 +0700

    Introduce LineStream

---
+++ src/system/line_stream.h
@@ -0,0 +1,13 @@
+#ifndef LINE_STREAM_H_
+#define LINE_STREAM_H_
+
+typedef struct LineStream LineStream;
+
+LineStream *create_line_stream(const char *filename,
+                               const char *mode,
+                               size_t capacity);
+void destroy_line_stream(LineStream *line_stream);
+
+const char *line_stream_next(LineStream *line_stream);
+
+#endif  // LINE_STREAM_H_
