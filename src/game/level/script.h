4550174e684e26724a8b49220e8535a6fd999ab6
rexim <reximkut@gmail.com>
2018-11-05 01:17:31 +0700

    TODO(#470)

--- src/game/level/script.h
+++ src/game/level/script.h
@@ -9,7 +9,7 @@ Script *create_script_from_line_stream(LineStream *line_stream,
                                        Level *level);
 void destroy_script(Script *script);
 
-// TODO: script_eval accepting string instead of expr is very error prone
+// TODO(#470): script_eval accepting string instead of expr is very error prone
 int script_eval(Script *script, const char *source_code);
 
 #endif  // SCRIPT_H_
