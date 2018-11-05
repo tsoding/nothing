e4015f5c8f9c159413ad83da22842265eb6c282b
rexim <reximkut@gmail.com>
2018-11-05 19:17:36 +0700

    TODO(#477)

--- src/system/nth_alloc.c
+++ src/system/nth_alloc.c
@@ -2,7 +2,7 @@
 #include "nth_alloc.h"
 #include "log.h"
 
-/* TODO: all instances of malloc should be replaced with nth_alloc (except ebisp) */
+/* TODO(#477): all instances of malloc should be replaced with nth_alloc (except ebisp) */
 void *nth_alloc(size_t size)
 {
     void *mem = malloc(size);
