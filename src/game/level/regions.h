68a459d9e5ac07e6c932b191d386247eb697a4b0
rexim <reximkut@gmail.com>
2018-11-11 00:12:09 +0700

    (#487) Show regions in the debug mode

--- src/game/level/regions.h
+++ src/game/level/regions.h
@@ -7,9 +7,12 @@ typedef struct Regions Regions;
 typedef struct Player Player;
 typedef struct LineStream LineStream;
 typedef struct Level Level;
+typedef struct Camera Camera;
 
 Regions *create_regions_from_line_stream(LineStream *line_stream, Level *level);
 void destroy_regions(Regions *regions);
+
+int regions_render(Regions *regions, Camera *camera);
 
 void regions_player_enter(Regions *regions, Player *player);
 void regions_player_leave(Regions *regions, Player *player);
