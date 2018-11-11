94ab6588940832df16f78dd0e8b7b563b5d20cf7
rexim <reximkut@gmail.com>
2018-11-12 00:40:47 +0700

    Add TODO(#500)

--- src/ebisp/expr.h
+++ src/ebisp/expr.h
@@ -33,7 +33,7 @@ struct Expr
     enum ExprType type;
     union {
         struct Cons *cons;
-        // TODO: Atom type is redundant
+        // TODO(#500): Atom type is redundant
         struct Atom *atom;
     };
 };
