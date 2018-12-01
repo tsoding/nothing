3e6fb9b3bf5a0e3cc8f4e61b7e5e6526ab956ccc
rexim <reximkut@gmail.com>
2018-12-02 00:02:29 +0700

    Remove TODO(#530)

--- src/ebisp/interpreter.c
+++ src/ebisp/interpreter.c
@@ -361,7 +361,6 @@ void load_std_library(Gc *gc, struct Scope *scope)
         NATIVE(gc, car, NULL));
 }
 
-/* TODO(#530): unpack_args doesn't support * format parameter */
 struct EvalResult
 unpack_args(struct Gc *gc, const char *format, struct Expr args, ...)
 {
