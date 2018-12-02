91f8c790925b3057044ee3f021fda4830368aa08
rexim <reximkut@gmail.com>
2018-12-02 13:41:35 +0700

    Remove TODO(#527)

--- levels/hide_jump_label.lisp
+++ levels/hide_jump_label.lisp
@@ -1,6 +1,5 @@
 (set on-enter
      (lambda ()
-       ;; TODO(#527): ebisp doesn't have `>`
        (when (> (get-player-jump-count) 1)
          (hide-goal "label_space_to_jump"))))
 (set on-leave
