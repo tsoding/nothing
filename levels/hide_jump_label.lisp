7a490f4c89276682e58cac35eee4e298b2964c21
rexim <reximkut@gmail.com>
2018-12-03 00:13:59 +0700

    (#522) Fix hide_jump_label.lisp script

--- levels/hide_jump_label.lisp
+++ levels/hide_jump_label.lisp
@@ -1,6 +1,6 @@
 (set on-enter
      (lambda ()
        (when (> (get-player-jump-count) 1)
-         (hide-goal "label_space_to_jump"))))
+         (hide-label "label_space_to_jump"))))
 (set on-leave
      (lambda ()))
