88de2bc295dec0b17304e54cfe92a2a4e6fe3597
rexim <reximkut@gmail.com>
2019-01-06 01:46:17 +0700

    Add TODO(#616)

--- levels/level-01/hide_label_double_jump.lisp
+++ levels/level-01/hide_label_double_jump.lisp
@@ -1,4 +1,4 @@
 (defun on-enter ()
-  ;; TODO: hiding labels is not implemented
+  ;; TODO(#616): hiding labels is not implemented
   (send `(level label "label_double_jump" hide)))
 (defun on-leave ())
