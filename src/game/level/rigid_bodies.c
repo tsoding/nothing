2ec7052caed56a65bcd6787edf6212eb3597cdea
rexim <reximkut@gmail.com>
2019-01-13 00:51:53 +0700

    (#628) add basic object functionality to RigidBodies

--- src/game/level/rigid_bodies.c
+++ src/game/level/rigid_bodies.c
@@ -3,6 +3,7 @@
 #include "system/lt.h"
 #include "system/nth_alloc.h"
 #include "system/stacktrace.h"
+#include "game/camera.h"
 
 #include "./rigid_bodies.h"
 
@@ -33,4 +34,41 @@ RigidBodies *create_rigid_bodies(void)
 void destroy_rigid_bodies(RigidBodies *rigid_bodies)
 {
     trace_assert(rigid_bodies);
+    RETURN_LT0(rigid_bodies->lt);
+}
+
+int rigid_bodies_update(RigidBodies *rigid_bodies,
+                        float delta_time)
+{
+    trace_assert(rigid_bodies);
+    (void) delta_time;
+    return 0;
+}
+
+int rigid_bodies_render(RigidBodies *rigid_bodies,
+                        Camera *camera)
+{
+    trace_assert(rigid_bodies);
+    trace_assert(camera);
+    return 0;
+}
+
+int rigid_bodies_add(RigidBodies *rigid_bodies,
+                     Rect rect,
+                     Color color,
+                     unsigned int *id)
+{
+    trace_assert(rigid_bodies);
+    trace_assert(id);
+    (void) rect;
+    (void) color;
+    return 0;
+}
+
+int rigid_bodies_remove(RigidBodies *rigid_bodies,
+                        unsigned int id)
+{
+    trace_assert(rigid_bodies);
+    (void) id;
+    return 0;
 }
