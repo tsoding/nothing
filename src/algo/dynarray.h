b247caf991a6a4bdcee6bef01d72a7a8c62cb5cf
rexim <reximkut@gmail.com>
2019-01-14 01:41:07 +0700

    Add TODO(#638)

--- src/algo/dynarray.h
+++ src/algo/dynarray.h
@@ -5,7 +5,7 @@ void *create_dynarray(size_t size);
 void destroy_dynarray(void *dynarray);
 
 size_t dynarray_count(void *dynarray);
-// TODO: dynarray_push result makes dynarrays not Lt friendly
+// TODO(#638): dynarray_push result makes dynarrays not Lt friendly
 void *dynarray_push(void *dynarray, void *elem);
 
 #endif  // DYNARRAY_H_
