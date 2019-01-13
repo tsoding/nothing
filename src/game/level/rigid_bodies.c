be9932f43d3929ec3fbf01da9a2e25e814321ee6
rexim <reximkut@gmail.com>
2019-01-14 01:28:06 +0700

    (#635) Implement ctor and dtor of rigid_bodies

--- src/game/level/rigid_bodies.c
+++ src/game/level/rigid_bodies.c
@@ -4,6 +4,7 @@
 #include "system/nth_alloc.h"
 #include "system/stacktrace.h"
 #include "game/camera.h"
+#include "algo/dynarray.h"
 
 #include "./rigid_bodies.h"
 
@@ -11,32 +12,99 @@
 
 struct RigidBodies
 {
-    Lt *lt;
+    Vec *positions;
+    Vec *velocities;
+    Vec *movements;
+    Vec *sizes;
+    Color *colors;
+    int *ground_touches;
+    Vec *forces;
 };
 
 RigidBodies *create_rigid_bodies(void)
 {
-    Lt *lt = create_lt();
-    if (lt == NULL) {
-        return NULL;
+    RigidBodies *rigid_bodies = nth_calloc(1, sizeof(RigidBodies));
+    if (rigid_bodies == NULL) {
+        goto fail;
     }
 
-    RigidBodies *rigid_bodies = PUSH_LT(
-        lt,
-        nth_alloc(sizeof(RigidBodies)),
-        free);
-    if (rigid_bodies == NULL) {
-        RETURN_LT(lt, NULL);
+    rigid_bodies->positions = create_dynarray(sizeof(Vec));
+    if (rigid_bodies->positions == NULL) {
+        goto fail;
     }
-    rigid_bodies->lt = lt;
+
+    rigid_bodies->velocities = create_dynarray(sizeof(Vec));
+    if (rigid_bodies->velocities == NULL) {
+        goto fail;
+    }
+
+    rigid_bodies->movements = create_dynarray(sizeof(Vec));
+    if (rigid_bodies->movements == NULL) {
+        goto fail;
+    }
+
+    rigid_bodies->sizes = create_dynarray(sizeof(Vec));
+    if (rigid_bodies->sizes == NULL) {
+        goto fail;
+    }
+
+    rigid_bodies->colors = create_dynarray(sizeof(Color));
+    if (rigid_bodies->colors == NULL) {
+        goto fail;
+    }
+
+    rigid_bodies->ground_touches = create_dynarray(sizeof(int));
+    if (rigid_bodies->ground_touches == NULL) {
+        goto fail;
+    }
+
+    rigid_bodies->forces = create_dynarray(sizeof(Vec));
+    if (rigid_bodies->forces == NULL) {
+        goto fail;
+    }
 
     return rigid_bodies;
+
+fail:
+    destroy_rigid_bodies(rigid_bodies);
+    return NULL;
 }
 
 void destroy_rigid_bodies(RigidBodies *rigid_bodies)
 {
-    trace_assert(rigid_bodies);
-    RETURN_LT0(rigid_bodies->lt);
+    if (rigid_bodies == NULL) {
+        return;
+    }
+
+    if (rigid_bodies->positions != NULL) {
+        destroy_dynarray(rigid_bodies->positions);
+    }
+
+    if (rigid_bodies->velocities != NULL) {
+        destroy_dynarray(rigid_bodies->velocities);
+    }
+
+    if (rigid_bodies->movements != NULL) {
+        destroy_dynarray(rigid_bodies->movements);
+    }
+
+    if (rigid_bodies->sizes != NULL) {
+        destroy_dynarray(rigid_bodies->sizes);
+    }
+
+    if (rigid_bodies->colors != NULL) {
+        destroy_dynarray(rigid_bodies->colors);
+    }
+
+    if (rigid_bodies->ground_touches != NULL) {
+        destroy_dynarray(rigid_bodies->ground_touches);
+    }
+
+    if (rigid_bodies->forces != NULL) {
+        destroy_dynarray(rigid_bodies->forces);
+    }
+
+    free(rigid_bodies);
 }
 
 int rigid_bodies_update(RigidBodies *rigid_bodies,
