c2fcb7982642ddc17370115a25dbf28d1e39eaba
rexim <reximkut@gmail.com>
2019-02-03 01:22:14 +0700

    (#625) Make baker generate includable header

--- src/ebisp/baker.c
+++ src/ebisp/baker.c
@@ -5,21 +5,35 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2) {
-        fprintf(stderr, "Using: ./baker <file.lisp>");
+    if (argc < 4) {
+        fprintf(stderr, "Using: baker <file.lisp> <output.h> <function-name>");
         return -1;
     }
 
-    const char *filename = argv[1];
+    const char *input = argv[1];
+    const char *output = argv[2];
+    const char *function_name = argv[3];
 
     Gc *gc = create_gc();
-    struct ParseResult result = read_all_exprs_from_file(gc, filename);
+    struct ParseResult result = read_all_exprs_from_file(gc, input);
     if (result.is_error) {
-        fprintf(stderr, "%s\n", result.error_message);
+        fprintf(stderr, "Parsing Error: %s\n", result.error_message);
         return -1;
     }
 
-    print_expr_as_c(stderr, result.expr);
+    FILE *output_file = fopen(output, "w+");
+    if (output_file == NULL) {
+        fprintf(stderr, "Printing Error: Could not open file %s\n", output);
+        return -1;
+    }
+
+    fprintf(output_file, "static struct Expr %s(Gc *gc) {\n", function_name);
+    fprintf(output_file, "    return ");
+    print_expr_as_c(output_file, result.expr);
+    fprintf(output_file, ";\n");
+    fprintf(output_file, "}\n");
+
+    fclose(output_file);
 
     return 0;
 }
