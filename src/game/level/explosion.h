393d650a886af849713a45b198af987244a177ce
rexim <reximkut@gmail.com>
2019-02-17 03:48:36 +0700

    (#691) dying_rect -> explosion

---
+++ src/game/level/explosion.h
@@ -0,0 +1,25 @@
+#ifndef EXPLOSION_H_
+#define EXPLOSION_H_
+
+#include <SDL2/SDL.h>
+
+#include "color.h"
+#include "game/camera.h"
+#include "math/rect.h"
+
+typedef struct Explosion Explosion;
+
+Explosion *create_explosion(Color color,
+                                float duration);
+void destroy_explosion(Explosion *explosion);
+
+int explosion_render(const Explosion *explosion,
+                     Camera *camera);
+int explosion_update(Explosion *explosion,
+                     float delta_time);
+
+int explosion_is_done(const Explosion *explosion);
+
+void explosion_start(Explosion *explosion, Vec position);
+
+#endif  // EXPLOSION_H_
