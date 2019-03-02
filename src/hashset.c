a39ac917365a8ccfd01319b265a03e4f22749e0b
rexim <reximkut@gmail.com>
2019-03-03 01:22:03 +0700

    (#709) Introduce HashSet

---
+++ src/hashset.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+
+#include "system/lt.h"
+#include "system/stacktrace.h"
+#include "system/nth_alloc.h"
+#include "linked_list.h"
+#include "hashset.h"
+
+struct HashSet
+{
+    Lt *lt;
+    size_t n;
+    size_t element_size;
+    LinkedList **buckets;
+};
+
+HashSet *create_hashset(size_t element_size, size_t n)
+{
+    Lt *lt = create_lt();
+    if (lt == NULL) {
+        return NULL;
+    }
+
+    HashSet *hash_set = PUSH_LT(lt, nth_alloc(sizeof(HashSet)), free);
+    if (hash_set == NULL) {
+        RETURN_LT(lt, NULL);
+    }
+    hash_set->lt = lt;
+
+    hash_set->n = n;
+    hash_set->element_size = element_size;
+
+    hash_set->buckets = PUSH_LT(lt, nth_calloc(n, sizeof(LinkedList*)), free);
+    if (hash_set->buckets == NULL) {
+        RETURN_LT(lt, NULL);
+    }
+
+    for (size_t i = 0; i < n; ++i) {
+        hash_set->buckets[i] = PUSH_LT(
+            lt,
+            create_linked_list(element_size),
+            destroy_linked_list);
+        if (hash_set->buckets[i] == NULL) {
+            RETURN_LT(lt, NULL);
+        }
+    }
+
+    return hash_set;
+}
+
+void destroy_hashset(HashSet *hashset)
+{
+    trace_assert(hashset);
+    RETURN_LT0(hashset->lt);
+}
+
+int hashset_insert(HashSet *hashset, void *element)
+{
+    trace_assert(hashset);
+    trace_assert(element);
+    return 0;
+}
+
+int hashset_remove(HashSet *hashset, void *element)
+{
+    trace_assert(hashset);
+    trace_assert(element);
+    return 0;
+}
+
+int hashset_contains(HashSet *hashset, void *element)
+{
+    trace_assert(hashset);
+    trace_assert(element);
+    return 0;
+}
+
+void hashset_clear(HashSet *hashset)
+{
+    trace_assert(hashset);
+}
---
+++ src/hashset.h
@@ -0,0 +1,14 @@
+#ifndef HASHSET_H_
+#define HASHSET_H_
+
+typedef struct HashSet HashSet;
+
+HashSet *create_hashset(size_t element_size, size_t n);
+void destroy_hashset(HashSet *hashset);
+
+int hashset_insert(HashSet *hashset, void *element);
+int hashset_remove(HashSet *hashset, void *element);
+int hashset_contains(HashSet *hashset, void *element);
+void hashset_clear(HashSet *hashset);
+
+#endif  // HASHSET_H_
