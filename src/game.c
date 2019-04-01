2b18ce028ba9fc03c82d549e1ed82d8c45c4adbd
rexim <reximkut@gmail.com>
2019-04-01 19:22:14 +0700

    Add TODO(#773)

--- src/game.c
+++ src/game.c
@@ -123,6 +123,7 @@ Game *create_game(const char *level_folder,
         lt,
         texture_from_bmp("images/cursor.bmp", renderer),
         SDL_DestroyTexture);
+    /* TODO(#773): If a specific building is not supported that should not crash the entire game */
     if (SDL_SetTextureBlendMode(
             game->texture_cursor,
             SDL_ComposeCustomBlendMode(
