45e86d347ba340f05567f583767e5250840c8174
rexim <reximkut@gmail.com>
2019-04-22 01:00:21 +0700

    (#805) Migrate Boxes from raw C arrays to Dynarray

--- src/dynarray.h
+++ src/dynarray.h
@@ -11,9 +11,11 @@ void destroy_dynarray(Dynarray *dynarray);
 size_t dynarray_count(const Dynarray *dynarray);
 void *dynarray_data(Dynarray *dynarray);
 void dynarray_clear(Dynarray *dynarray);
+// O(1) amortized
 int dynarray_push(Dynarray *dynarray, const void *element);
 bool dynarray_contains(const Dynarray *dynarray,
                        const void *element);
+// O(N)
 void dynarray_delete_at(Dynarray *dynarray, size_t index);
 
 #endif  // DYNARRAY_H_
