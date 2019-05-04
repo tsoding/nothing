fbab88099d6dbaf5422ccdf6421bb64063ac293b
rexim <reximkut@gmail.com>
2019-05-05 02:47:18 +0700

    Add TODO(#836)

--- src/game/level/level_editor/layer.h
+++ src/game/level/level_editor/layer.h
@@ -1,7 +1,7 @@
 #ifndef LAYER_H_
 #define LAYER_H_
 
-// TODO: Layer should be called RectLayer
+// TODO(#836): Layer should be called RectLayer
 typedef struct Layer Layer;
 typedef struct LineStream LineStream;
 
