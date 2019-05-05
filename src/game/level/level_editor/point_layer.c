530abff849ab091513f713d751d58df83d6b8be2
rexim <reximkut@gmail.com>
2019-05-06 00:13:41 +0700

    Add TODO(#841)

--- src/game/level/level_editor/point_layer.c
+++ src/game/level/level_editor/point_layer.c
@@ -112,7 +112,7 @@ int point_layer_render(const PointLayer *point_layer,
     return 0;
 }
 
-// TODO: PointLayer does not allow to remove elements
+// TODO(#841): PointLayer does not allow to remove elements
 
 int point_layer_mouse_button(PointLayer *point_layer,
                              const SDL_MouseButtonEvent *event,
