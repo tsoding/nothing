c8ad98bcee7926488d056c4a4065258a3ba24635
rexim <reximkut@gmail.com>
2019-05-13 00:30:18 +0700

    Introduce new Lt

--- src/system/lt.h
+++ src/system/lt.h
@@ -1,50 +1,112 @@
 #ifndef LT_H_
 #define LT_H_
 
-#define PUSH_LT(lt, resource, resource_destroy) \
-    lt_push(lt, (void*)resource, (Lt_destroy)resource_destroy)
+#include <assert.h>
+#include <string.h>
 
-#define RESET_LT(lt, old_resource, new_resource)    \
-    lt_reset(lt, (void*)old_resource, (void*)new_resource)
+#define LT_INITIAL_CAPACITY 2
 
-#define REPLACE_LT(lt, old_resource, new_resource)  \
-    lt_replace(lt, (void*)old_resource, (void*)new_resource)
+typedef void (*Dtor)(void*);
 
-#define RELEASE_LT(lt, resource)                \
-    lt_release(lt, (void*) resource)
+typedef struct {
+    void *res;
+    Dtor dtor;
+} Slot;
 
-#define RETURN_LT(lt, result)               \
-    do {                                    \
-        destroy_lt(lt);                     \
-        return result;                      \
-    } while (0)
+typedef struct {
+    size_t size;
+    size_t capacity;
+    Slot *slots;
+} Lt;
 
-#define RETURN_LT0(lt)                          \
-    do {                                        \
-        destroy_lt(lt);                         \
-        return;                                 \
-    } while (0)
+static inline void destroy_lt(Lt lt)
+{
+    for (size_t i = lt.size; i > 0; --i) {
+        if (lt.slots[i - 1].res) {
+            lt.slots[i - 1].dtor(lt.slots[i - 1].res);
+        }
+    }
 
-typedef struct Lt Lt;
-typedef void (*Lt_destroy)(void*);
+    free(lt.slots);
+}
 
-Lt *create_lt(void);
-void destroy_lt(Lt *lt);
+#define PUSH_LT(lt, res, dtor)                  \
+    lt_push(&lt, (void*)res, (Dtor)dtor)
 
-/** \brief Pushes the resource onto the Life Time creating a new Life Time frame.
- */
-void *lt_push(Lt *lt, void *resource, Lt_destroy resource_destroy);
+static inline void *lt_push(Lt *lt, void *res, Dtor dtor)
+{
+    if (lt->size >= lt->capacity) {
+        if (lt->capacity == 0) {
+            lt->capacity = LT_INITIAL_CAPACITY;
+            lt->slots = calloc(LT_INITIAL_CAPACITY, sizeof(Slot));
+        } else {
+            lt->capacity *= 2;
+            lt->slots = realloc(lt->slots, lt->capacity * sizeof(Slot));
+        }
+    }
 
-/** \brief Destroys old_resource preserving its LT frame and assigns the new_resource to the LT frame.
- */
-void *lt_reset(Lt *lt, void *old_resource, void *new_resource);
+    lt->slots[lt->size].res = res;
+    lt->slots[lt->size].dtor = dtor;
+    lt->size++;
 
-/** \brief Replaces old_resource with new_resource in the LT frame without destroying old_resource.
- */
-void *lt_replace(Lt *lt, void *old_resource, void *new_resource);
+    return res;
+}
 
-/** \brief Removes the LT frame of resource without destroying the resource.
- */
-void *lt_release(Lt *lt, void *resource);
+#define RETURN_LT(lt, result)                   \
+    return (destroy_lt(lt), result)
+
+#define RETURN_LT0(lt) (destroy_lt(lt), return)
+
+#define RESET_LT(lt, old_res, new_res)          \
+    lt_reset(&lt, (void*) old_res, (void*) new_res)
+
+static inline void *lt_reset(Lt *lt, void *old_res, void *new_res)
+{
+    assert(old_res != new_res);
+
+    for (size_t i = 0; i < lt->size; ++i) {
+        if (lt->slots[i].res == old_res) {
+            lt->slots[i].dtor(old_res);
+            lt->slots[i].res = new_res;
+            return new_res;
+        }
+    }
+    return NULL;
+}
+
+
+#define REPLACE_LT(lt, old_res, new_res)        \
+    lt_replace(&lt, (void *)old_res, (void*)new_res)
+
+static inline void *lt_replace(Lt *lt, void *old_res, void *new_res)
+{
+    for (size_t i = 0; i < lt->size; ++i) {
+        if (lt->slots[i].res == old_res) {
+            lt->slots[i].res = new_res;
+            return new_res;
+        }
+    }
+
+    return NULL;
+}
+
+#define RELEASE_LT(lt, res)                     \
+    lt_release(&lt, (void*)res)
+
+static inline void *lt_release(Lt *lt, void *res)
+{
+    for (size_t i = 0; i < lt->size; ++i) {
+        if (lt->slots[i].res == res) {
+            memmove(
+                &lt->slots[i],
+                &lt->slots[i + 1],
+                (lt->size - i - 1) * sizeof(Slot));
+            lt->size--;
+            return res;
+        }
+    }
+
+    return NULL;
+}
 
 #endif  // LT_H_
