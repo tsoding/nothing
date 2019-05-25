e32d7907e096bb86433838b2e6101876455b2364
rexim <reximkut@gmail.com>
2019-05-26 00:08:43 +0700

    (#845) Center Layer Picker vertically

--- src/game/level/level_editor/layer_picker.c
+++ src/game/level/level_editor/layer_picker.c
@@ -14,8 +14,6 @@
 #define LAYER_CELL_WIDTH (200.0f + LAYER_TITLE_PADDING * 2.0f)
 #define LAYER_CELL_HEIGHT (LAYER_TITLE_SIZE * FONT_CHAR_HEIGHT + LAYER_TITLE_PADDING * 2.0f)
 #define SELECTOR_HEIGHT (LAYER_CELL_HEIGHT * 0.20f)
-#define LAYER_PICKER_POS_X 0.0f
-#define LAYER_PICKER_POS_Y (LAYER_CELL_HEIGHT + 500.0f)
 
 /* TODO: introduce Button "class" */
 
@@ -28,12 +26,26 @@ static const Color LAYER_CELL_BACKGROUND_COLORS[LAYER_PICKER_N] = {
 };
 
 static const char *LAYER_CELL_TITLES[LAYER_PICKER_N] = {
-    "Boxes",                    // LAYER_PICKER_BOXES = 0,
-    "Platforms",                // LAYER_PICKER_PLATFORMS,
-    "Back Platforms",           // LAYER_PICKER_BACK_PLATFORMS,
-    "Goals",                    // LAYER_PICKER_GOALS
-    "Player",                   // LAYER_PICKER_PLAYER
+    "Boxes",                   // LAYER_PICKER_BOXES = 0,
+    "Platforms",               // LAYER_PICKER_PLATFORMS,
+    "Back Platforms",          // LAYER_PICKER_BACK_PLATFORMS,
+    "Goals",                   // LAYER_PICKER_GOALS
+    "Player",                  // LAYER_PICKER_PLAYER
 };
+
+inline static Vec layer_picker_position(const Camera *camera)
+{
+    trace_assert(camera);
+
+    const Rect viewport = camera_view_port_screen(camera);
+
+    Vec position = {
+        .x = 0.0f,
+        .y = viewport.h * 0.5f - LAYER_CELL_HEIGHT * LAYER_PICKER_N * 0.5f
+    };
+
+    return position;
+}
 
 int layer_picker_render(const LayerPicker *layer_picker,
                         Camera *camera)
@@ -41,12 +53,14 @@ int layer_picker_render(const LayerPicker *layer_picker,
     trace_assert(layer_picker);
     trace_assert(camera);
 
+    const Vec position = layer_picker_position(camera);
+
     for (size_t i = 0; i < LAYER_PICKER_N; ++i) {
         if (camera_fill_rect_screen(
                 camera,
                 rect(
-                    LAYER_PICKER_POS_X,
-                    LAYER_CELL_HEIGHT * (float) i + LAYER_PICKER_POS_Y,
+                    position.x,
+                    LAYER_CELL_HEIGHT * (float) i + position.y,
                     LAYER_CELL_WIDTH,
                     LAYER_CELL_HEIGHT),
                 LAYER_CELL_BACKGROUND_COLORS[i]) < 0) {
@@ -58,16 +72,16 @@ int layer_picker_render(const LayerPicker *layer_picker,
                 LAYER_CELL_TITLES[i],
                 vec(LAYER_TITLE_SIZE, LAYER_TITLE_SIZE),
                 color_invert(LAYER_CELL_BACKGROUND_COLORS[i]),
-                vec(LAYER_PICKER_POS_X + LAYER_TITLE_PADDING,
-                    LAYER_CELL_HEIGHT * (float) i + LAYER_PICKER_POS_Y + LAYER_TITLE_PADDING)) < 0) {
+                vec(position.x + LAYER_TITLE_PADDING,
+                    LAYER_CELL_HEIGHT * (float) i + position.y + LAYER_TITLE_PADDING)) < 0) {
             return -1;
         }
 
         if (*layer_picker == i) {
             if (camera_fill_rect_screen(
                     camera,
-                    rect(LAYER_PICKER_POS_X,
-                         LAYER_PICKER_POS_Y + LAYER_CELL_HEIGHT * (float) (i + 1) - SELECTOR_HEIGHT,
+                    rect(position.x,
+                         position.y + LAYER_CELL_HEIGHT * (float) (i + 1) - SELECTOR_HEIGHT,
                          LAYER_CELL_WIDTH,
                          SELECTOR_HEIGHT),
                     color_invert(LAYER_CELL_BACKGROUND_COLORS[i])) < 0) {
@@ -88,11 +102,13 @@ int layer_picker_event(LayerPicker *layer_picker,
     trace_assert(event);
     trace_assert(camera);
 
+    const Vec position = layer_picker_position(camera);
+
     switch (event->type) {
     case SDL_MOUSEBUTTONDOWN: {
         for (size_t i = 0; i < LAYER_PICKER_N; ++i) {
-            const Rect cell = rect(LAYER_PICKER_POS_X,
-                                   LAYER_CELL_HEIGHT * (float) i + LAYER_PICKER_POS_Y,
+            const Rect cell = rect(position.x,
+                                   LAYER_CELL_HEIGHT * (float) i + position.y,
                                    LAYER_CELL_WIDTH,
                                    LAYER_CELL_HEIGHT);
             if (rect_contains_point(cell, vec((float) event->button.x, (float) event->button.y))) {
