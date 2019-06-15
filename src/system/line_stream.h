9d9a8f58918ba634beec8fc57b4f9771d66e2778
rexim <reximkut@gmail.com>
2019-06-16 00:24:04 +0700

    (#885) Remove amount of supa script lines from the level format

--- src/system/line_stream.h
+++ src/system/line_stream.h
@@ -13,5 +13,6 @@ void destroy_line_stream(LineStream *line_stream);
 const char *line_stream_next_chunk(LineStream *line_stream);
 const char *line_stream_next(LineStream *line_stream);
 char *line_stream_collect_n_lines(LineStream *line_stream, size_t n);
+char *line_stream_collect_until_end(LineStream *line_stream);
 
 #endif  // LINE_STREAM_H_
