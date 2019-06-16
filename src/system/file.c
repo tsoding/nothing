27a5d39503fbeba87d6ee87541df79458ea0478b
rexim <reximkut@gmail.com>
2019-06-16 21:52:30 +0700

    Add TODO(#901)

--- src/system/file.c
+++ src/system/file.c
@@ -62,7 +62,7 @@ int last_modified(const char *filepath, time_t *time)
 
 #elif defined(__APPLE__)
 
-    // TODO: implement last_modified for Mac OS X
+    // TODO(#901): implement last_modified for Mac OS X
     #warning last_modified is not implemented
     return -1;
 
