3c99725405bcf7524f4e885f8b3f8864c8894961
rexim <reximkut@gmail.com>
2019-07-28 00:05:26 +0700

    (#963) Implement dynarray_push_empty

--- src/dynarray.h
+++ src/dynarray.h
@@ -11,8 +11,10 @@ void destroy_dynarray(Dynarray *dynarray);
 size_t dynarray_count(const Dynarray *dynarray);
 void *dynarray_data(Dynarray *dynarray);
 void dynarray_clear(Dynarray *dynarray);
+// TODO: dynarray_push should be called dynarray_push_copy
 // O(1) amortized
 int dynarray_push(Dynarray *dynarray, const void *element);
+int dynarray_push_empty(Dynarray *dynarray);
 bool dynarray_contains(const Dynarray *dynarray,
                        const void *element);
 // O(N)
