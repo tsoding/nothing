2d6873c9b41b09b51e2c60e85da41e426cdba155
rexim <reximkut@gmail.com>
2019-08-03 23:32:21 +0700

    Remove TODO(#985)

--- src/game/level/level_editor/point_layer.c
+++ src/game/level/level_editor/point_layer.c
@@ -19,7 +19,6 @@
 #define POINT_LAYER_ID_TEXT_SIZE vec(2.0f, 2.0f)
 #define POINT_LAYER_ID_TEXT_COLOR COLOR_BLACK
 
-// TODO(#985): PointLayer cannot move its points
 typedef enum {
     POINT_LAYER_IDLE = 0,
     POINT_LAYER_EDIT_ID,
