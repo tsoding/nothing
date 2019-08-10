64aced305356dd96053b71b9cad284cfacd7e463
rexim <reximkut@gmail.com>
2019-08-11 01:36:16 +0700

    (#1002) Introduce BackgroundLayer

---
+++ src/game/level/level_editor/background_layer.h
@@ -0,0 +1,20 @@
+#ifndef BACKGROUND_LAYER_H_
+#define BACKGROUND_LAYER_H_
+
+#include "color_picker.h"
+
+typedef struct {
+    ColorPicker color_picker;
+} BackgroundLayer;
+
+int background_layer_render(BackgroundLayer *layer,
+                            Camera *camera,
+                            int active);
+int background_layer_event(BackgroundLayer *layer,
+                           const SDL_Event *event,
+                           const Camera *camera,
+                           UndoHistory *undo_history);
+int background_dump_stream(BackgroundLayer *layer,
+                           FILE *stream);
+
+#endif  // BACKGROUND_LAYER_H_
