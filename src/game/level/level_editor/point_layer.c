3cab1acf3a5904162bec2f12a30dde3692b1be8d
rexim <reximkut@gmail.com>
2019-09-23 02:32:19 +0700

    (#1075) Get rid of layer arg from UNDO_PUSH in PointLayer

--- src/game/level/level_editor/point_layer.c
+++ src/game/level/level_editor/point_layer.c
@@ -117,15 +117,16 @@ void point_layer_undo(void *context, size_t context_size)
     }
 }
 
-#define UNDO_PUSH(LAYER, HISTORY, UNDO_TYPE)                            \
+#define UNDO_PUSH(HISTORY, CONTEXT)                                     \
     do {                                                                \
-        UndoContext context = point_layer_create_undo_context(LAYER, UNDO_TYPE); \
+        UndoContext context = (CONTEXT);                                \
         undo_history_push(                                              \
             HISTORY,                                                    \
             point_layer_undo,                                           \
             &context,                                                   \
             sizeof(context));                                           \
     } while(0)
+
 
 LayerPtr point_layer_as_layer(PointLayer *point_layer)
 {
@@ -345,7 +346,9 @@ int point_layer_add_element(PointLayer *point_layer,
     dynarray_push(point_layer->colors, &color);
     dynarray_push(point_layer->ids, id);
 
-    UNDO_PUSH(point_layer, undo_history, UNDO_ADD);
+    UNDO_PUSH(
+        undo_history,
+        point_layer_create_undo_context(point_layer, UNDO_ADD));
 
     return 0;
 }
@@ -357,7 +360,11 @@ void point_layer_delete_nth_element(PointLayer *point_layer,
 {
     trace_assert(point_layer);
 
-    UNDO_PUSH(point_layer, undo_history, UNDO_DELETE);
+    UNDO_PUSH(
+        undo_history,
+        point_layer_create_undo_context(
+            point_layer,
+            UNDO_DELETE));
 
     dynarray_delete_at(point_layer->positions, i);
     dynarray_delete_at(point_layer->colors, i);
@@ -483,7 +490,11 @@ int point_layer_edit_id_event(PointLayer *point_layer,
     case SDL_KEYDOWN: {
         switch(event->key.keysym.sym) {
         case SDLK_RETURN: {
-            UNDO_PUSH(point_layer, undo_history, UNDO_UPDATE);
+            UNDO_PUSH(
+                undo_history,
+                point_layer_create_undo_context(
+                    point_layer,
+                    UNDO_UPDATE));
 
             char *id = dynarray_pointer_at(point_layer->ids, (size_t) point_layer->selected);
             const char *text = edit_field_as_text(point_layer->edit_field);
@@ -526,7 +537,11 @@ int point_layer_move_event(PointLayer *point_layer,
             point_layer->state = POINT_LAYER_IDLE;
 
             // TODO(#1014): just click (without moving) on the point creates an undo history entry
-            UNDO_PUSH(point_layer, undo_history, UNDO_UPDATE);
+            UNDO_PUSH(
+                undo_history,
+                point_layer_create_undo_context(
+                    point_layer,
+                    UNDO_UPDATE));
 
             dynarray_replace_at(
                 point_layer->positions,
@@ -570,7 +585,11 @@ int point_layer_recolor_event(PointLayer *point_layer,
         point_layer->inter_color = color_picker_rgba(&point_layer->color_picker);
 
         if (!color_picker_drag(&point_layer->color_picker)) {
-            UNDO_PUSH(point_layer, undo_history, UNDO_UPDATE);
+            UNDO_PUSH(
+                undo_history,
+                point_layer_create_undo_context(
+                    point_layer,
+                    UNDO_UPDATE));
 
             dynarray_replace_at(
                 point_layer->colors,
