5c25a5173a8ee0d3b49f20f8b7a6f9a2bb4bfcf7
rexim <reximkut@gmail.com>
2019-10-06 02:29:29 +0700

    (#330) TODO

--- src/ebisp/interpreter.c
+++ src/ebisp/interpreter.c
@@ -263,6 +263,8 @@ match_list(struct Gc *gc, const char *format, struct Expr xs, ...)
     va_list args_list;
     va_start(args_list, xs);
 
+    // TODO: match_list does not support real numbers
+
     long int i = 0;
     for (i = 0; *format != 0 && !nil_p(xs); ++i) {
         if (!cons_p(xs)) {
