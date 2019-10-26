b322dbaebadc47c6c9acae4144842bf40575e1fb
rexim <reximkut@gmail.com>
2019-10-27 01:18:35 +0700

    (#1108) Fix const correctness in camera_draw_rect_screen

--- src/game/camera.c
+++ src/game/camera.c
@@ -87,7 +87,7 @@ int camera_draw_rect(const Camera *camera,
     return 0;
 }
 
-int camera_draw_rect_screen(Camera *camera,
+int camera_draw_rect_screen(const Camera *camera,
                             Rect rect,
                             Color color)
 {
