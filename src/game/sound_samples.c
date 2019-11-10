feaf1e28e9ef3bf27ad19fdaee5a9504b6c4635d
rexim <reximkut@gmail.com>
2019-11-10 23:32:26 +0700

    (#1125) Fix fomatting

--- src/game/sound_samples.c
+++ src/game/sound_samples.c
@@ -25,9 +25,9 @@ struct Sound_samples
     int failed;                 // This is hackish
 };
 
-static 
-int init_buffer_and_device(Sound_samples *sound_samples, 
-                           const char *sample_files[]) 
+static
+int init_buffer_and_device(Sound_samples *sound_samples,
+                           const char *sample_files[])
 {
     // TODO(#1023): init_buffer_and_device uses hard-coded audio specification
     SDL_AudioSpec destination_spec = { // stereo float32 44100Hz
@@ -40,7 +40,7 @@ int init_buffer_and_device(Sound_samples *sound_samples,
         log_fail("No audio in 2019 LULW\n");
         return -1;
     }
-    
+
     sound_samples->audio_buf_array = PUSH_LT(sound_samples->lt, nth_calloc(sound_samples->samples_count, sizeof(uint8_t*)), free);
     if (sound_samples->audio_buf_array == NULL) {
         log_fail("Failed to allocate memory for audio buffer pointer array\n");
@@ -61,7 +61,7 @@ int init_buffer_and_device(Sound_samples *sound_samples,
         }
         PUSH_LT(sound_samples->lt, wav_buf, SDL_FreeWAV);
         SDL_AudioCVT cvt;
-        int result = SDL_BuildAudioCVT(&cvt, wav_spec.format, (uint8_t)wav_spec.channels, (int)wav_spec.freq, 
+        int result = SDL_BuildAudioCVT(&cvt, wav_spec.format, (uint8_t)wav_spec.channels, (int)wav_spec.freq,
                           destination_spec.format, (uint8_t)destination_spec.channels, (int)destination_spec.freq);
         if (result < 0) {
             log_fail("SDL_BuildAudioCVT failed: %s\n", SDL_GetError());
@@ -86,7 +86,7 @@ int init_buffer_and_device(Sound_samples *sound_samples,
             sound_samples->audio_buf_size_array[i] = (uint32_t)cvt.len_cvt;
         }
     }
-    
+
     /* Allocating active audio buffer location*/
     //TODO(#1072): Allocate one huge active audio buffer with length of the maximum of all audio buffer, instead of one active buffer for each audio
     sound_samples->active_audio_buf_array = PUSH_LT(sound_samples->lt, nth_calloc(sound_samples->samples_count, sizeof(uint8_t*)), free);
