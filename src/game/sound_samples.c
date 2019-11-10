9a54dc4947b33a157107eca65db994949ce4c508
rexim <reximkut@gmail.com>
2019-11-10 23:24:50 +0700

    (#1125) Allow to play the game with failed sound samples

--- src/game/sound_samples.c
+++ src/game/sound_samples.c
@@ -21,6 +21,8 @@ struct Sound_samples
     size_t samples_count;
     int paused;
     float volume;
+    // TODO: A better solution for optional sound support
+    int failed;                 // This is hackish
 };
 
 static 
@@ -129,7 +131,7 @@ Sound_samples *create_sound_samples(const char *sample_files[],
     sound_samples->samples_count = sample_files_count;
     if (init_buffer_and_device(sound_samples, sample_files) < 0) {
         log_fail("init_buffer_and_device failed\n");
-        RETURN_LT(lt, NULL);
+        sound_samples->failed = 1;
     }
 
     sound_samples->paused = 0;
@@ -153,6 +155,9 @@ int sound_samples_play_sound(Sound_samples *sound_samples,
     trace_assert(sound_samples);
     trace_assert(sound_index < sound_samples->samples_count);
     trace_assert(sound_samples->dev);
+
+    if (sound_samples->failed) return 0;
+
     /* Premix the audio volume */
     memset(sound_samples->active_audio_buf_array[sound_index], 0, sound_samples->audio_buf_size_array[sound_index]);
 
@@ -177,6 +182,7 @@ int sound_samples_play_sound(Sound_samples *sound_samples,
 int sound_samples_toggle_pause(Sound_samples *sound_samples)
 {
     trace_assert(sound_samples);
+    if (sound_samples->failed) return 0;
     sound_samples->paused = !sound_samples->paused;
     trace_assert(sound_samples->dev);
     SDL_PauseAudioDevice(sound_samples->dev, sound_samples->paused);
@@ -186,6 +192,7 @@ int sound_samples_toggle_pause(Sound_samples *sound_samples)
 void sound_samples_update_volume(Sound_samples *sound_samples,
                                  float volume)
 {
-  trace_assert(sound_samples);
-  sound_samples->volume = volume;
+    trace_assert(sound_samples);
+    if (sound_samples->failed) return;
+    sound_samples->volume = volume;
 }
