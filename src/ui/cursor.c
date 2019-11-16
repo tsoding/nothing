29605eb9254e9887b37a1fb52c2bd2239cf0c77b
rexim <reximkut@gmail.com>
2019-11-17 01:20:59 +0700

    (#1129) Introduce cursor icon pivot

--- src/ui/cursor.c
+++ src/ui/cursor.c
@@ -9,8 +9,14 @@ int cursor_render(const Cursor *cursor, SDL_Renderer *renderer)
     int cursor_x, cursor_y;
     SDL_GetMouseState(&cursor_x, &cursor_y);
 
-    const SDL_Rect src = {0, 0, 32, 32};
-    const SDL_Rect dest = {cursor_x, cursor_y, 32, 32};
+    const SDL_Rect src = {0, 0, CURSOR_ICON_WIDTH, CURSOR_ICON_HEIGHT};
+    const SDL_Rect dest = {
+        cursor_x - cursor_style_tex_pivots[cursor->style][0],
+        cursor_y - cursor_style_tex_pivots[cursor->style][1],
+        CURSOR_ICON_WIDTH,
+        CURSOR_ICON_HEIGHT
+    };
+
     if (SDL_RenderCopy(
             renderer,
             cursor->texs[cursor->style],
--- src/ui/cursor.h
+++ src/ui/cursor.h
@@ -3,6 +3,9 @@
 
 #include <SDL2/SDL.h>
 
+#define CURSOR_ICON_WIDTH 32
+#define CURSOR_ICON_HEIGHT 32
+
 typedef enum {
     CURSOR_STYLE_POINTER = 0,
     CURSOR_STYLE_RESIZE_VERT,
@@ -19,6 +22,14 @@ static const char * const cursor_style_tex_files[CURSOR_STYLE_N] = {
     "./assets/images/cursor-resize-horis.bmp",
     "./assets/images/cursor-resize-diag1.bmp",
     "./assets/images/cursor-resize-diag2.bmp"
+};
+
+static const int cursor_style_tex_pivots[CURSOR_STYLE_N][2] = {
+    {0, 0},
+    {CURSOR_ICON_WIDTH / 2, CURSOR_ICON_HEIGHT / 2},
+    {CURSOR_ICON_WIDTH / 2, CURSOR_ICON_HEIGHT / 2},
+    {CURSOR_ICON_WIDTH / 2, CURSOR_ICON_HEIGHT / 2},
+    {CURSOR_ICON_WIDTH / 2, CURSOR_ICON_HEIGHT / 2}
 };
 
 typedef struct {
