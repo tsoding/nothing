606da9fb4f0197d0ec38d07509674241120bfc47
rexim <reximkut@gmail.com>
2020-01-20 02:42:32 +0700

    Fix syntax error in check-format.sh

--- tools/check-format.sh
+++ tools/check-format.sh
@@ -2,9 +2,10 @@
 
 ./tools/clang-format-all.sh
 
-if [ ! -z $(git diff) ]; do
+if [ ! -z "$(git --no-pager diff)" ]; then
     echo "------------------------------"
     echo "FORMATTING IS INCORRECT"
     echo "------------------------------"
-    git diff
-done
+    git --no-pager diff
+    exit 1
+fi
